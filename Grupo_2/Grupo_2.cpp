49d61500648b49a84002f8da2137a7df598ec0a7
Akio Joaquin Quispe Layme <a20241546@pucp.edu.pe>
2026-08-24 15:20:04 -0500

    suma

--- Grupo_2/Grupo_2.cpp
+++ Grupo_2/Grupo_2.cpp
@@ -2,6 +2,18 @@
 
 using namespace System;
 
+public ref class sumar {
+public:
+    int a;
+    int b;
+    int sumarNumeros(int num1, int num2){
+        a = num1;
+        b = num2;
+        return a + b;
+    }
+
+};
+
 int main(array<System::String ^> ^args)
 {
     return 0;
