54ac53405de03f607802807cc6ae4993fecfc980
fernando-alf12 <a20240820@pucp.edu.pe>
2026-08-24 14:35:05 -0500

    mi clase bros

--- Grupo_2/Fernando-grupo2.cpp
+++ Grupo_2/Fernando-grupo2.cpp
@@ -1,5 +1,15 @@
 #include "PCH.H"
 using namespace System;
+
+public ref class Alumno {
+private:
+	String^ Fernando;
+	int edad;
+	float altura;
+};
+
+
+
 int mi_funcion_fernando() {
 	return 0;
 }
